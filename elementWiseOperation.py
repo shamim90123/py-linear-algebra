b6d0d8713fe6d17b1a24b0aa0ffde661b11e28c3
Md Shamim Reza <opt.shamim@gmail.com>
2024-01-13 12:24:37 +0600

    first commit

---
+++ elementWiseOperation.py
@@ -0,0 +1,19 @@
+
+import numpy as np
+# matrix = np.array([[1,2,3],[4,5,6]])
+# vector=np.array([7,9,6])
+
+# element-wise operation
+# print('matrix+vector:\n',matrix+vector)
+# print('matrix-vector:\n',matrix-vector)
+# print('matrix*vector:\n',matrix*vector)
+# print('matrix/vector:\n',matrix/vector)
+# print('matrix**vector:\n',matrix**vector)
+
+# for dot 
+matrix = np.array([[1,2,3],[4,5,6]])
+matrix2 = np.array([[1,2],[3,4],[5,6]])
+
+# matrix multiplication
+print('matrix.dot(matrix2):\n',matrix.dot(matrix2))
+
